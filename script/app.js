835caca777a4750c61c437b5574ad42b0aa0829f
Ali Muhammadi <alimmhd30@gmail.com>
2022-03-13 01:13:27 +0330

    change function

--- script/app.js
+++ script/app.js
@@ -2,6 +2,12 @@ const displayResult = document.querySelector(".calculator-screen");
 const outPutOperate = document.querySelector(".data-calculation");
 const numberData = document.querySelectorAll(".data-number");
 const btnKeys = document.querySelector(".calculator-keys");
+const historyMemory = document.querySelector(".sidebar");
+const historyView = document.querySelector(".history-calculation");
+const memoryView = document.querySelector(".memory-calculation");
+let id = 0;
+let memoryList = [];
+let historyList = [];
 const calculator = {
   displayValue: "0",
   firstNumber: null,
@@ -16,13 +22,14 @@ const inputDigit = (number) => {
   if (calculator.waitingForSecondOperand === true) {
     if (calculator.result !== null) {
       calculator.firstNumber = calculator.result;
-      calculator.secondNumber = null;
+      // calculator.secondNumber = null;
       calculator.displayValue = "0";
     }
     calculator.displayValue = "0";
     calculator.secondNumber === null
       ? (calculator.secondNumber = number)
       : (calculator.secondNumber += number);
+    console.log("lez-mez");
 
     calculator.displayValue = calculator.secondNumber;
   } else if (calculator.firstNumber === null) {
@@ -41,6 +48,18 @@ const inputDigit = (number) => {
   );
 };
 
+const deleteLastCharacter = () => {
+  displayResult.value = displayResult.value.substring(
+    0,
+    displayResult.value.length - 1
+  );
+  calculator.firstNumber = displayResult.value;
+  calculator.displayValue = displayResult.value;
+
+  // displayResult.val = displayResult.val().slice(0, -1);
+  updateDisplay();
+};
+
 function inputDecimal() {
   if (!displayResult.value.includes(".")) {
     displayResult.value += ".";
@@ -48,7 +67,6 @@ function inputDecimal() {
 }
 
 const positiveNegative = () => {
-  // debugger;
   if (displayResult.value.includes("-")) {
     let digit = String(displayResult.value);
     displayResult.value = digit.replace("-", "");
@@ -76,11 +94,15 @@ const operatorVal = (operatorValue) => {
     calculator.operator = operatorValue;
     if (calculator.result !== null) {
       calculator.firstNumber = calculator.result;
+    } else {
+      calculator.displayValue = calculator.firstNumber;
     }
     outPutOperate.innerHTML = ` ${calculator.firstNumber} ${calculator.operator} `;
   }
   calculator.waitingForSecondOperand = true;
-  calculator.displayValue = calculator.result;
+  if (calculator.result !== null) {
+    calculator.displayValue = calculator.result;
+  }
   updateDisplay();
 };
 
@@ -100,9 +122,37 @@ const resultOperator = () => {
     calculator.result =
       parseFloat(calculator.firstNumber) / parseFloat(calculator.secondNumber);
   }
+  calculator.secondNumber = null;
   calculator.displayValue = calculator.result;
+
+  // historyList.push(calculator.result, outPutOperate.innerHTML);
+  // console.log(historyList);
+  updateHistory();
   updateDisplay();
 };
+
+function updateHistoryView() {
+  historyView.innerHTML = "";
+
+  historyList.forEach((list) => {
+    console.log(list);
+    const lirrrr = document.createElement("li");
+    lirrrr.innerHTML = `${list.operationHistory} <br> ${list.resultHistory}`;
+    historyView.prepend(lirrrr);
+  });
+  console.log(historyView);
+}
+
+function updateHistory() {
+  const objHistory = {
+    idHistory: id++,
+    resultHistory: calculator.result,
+    operationHistory: outPutOperate.innerHTML,
+  };
+  historyList.push(objHistory);
+  console.log(historyList);
+  updateHistoryView();
+}
 
 function updateDisplay() {
   // if (calculator.displayValue === "0" || calculator.displayValue === "") {
@@ -140,15 +190,32 @@ btnKeys.addEventListener("click", (e) => {
     positiveNegative();
   }
 
-  if (e.target.classList.contains("clear-all")) {
+  if (e.target.classList.contains("clear-result")) {
     console.log(e.target.value);
   }
 
-  if (e.target.classList.contains("all-clear")) {
+  if (e.target.classList.contains("clear-all")) {
     console.log(e.target.value);
   }
 
   if (e.target.classList.contains("delete-number")) {
     console.log(e.target.value);
+    deleteLastCharacter();
+  }
+});
+
+historyMemory.addEventListener("click", (e) => {
+  console.log("pashmak");
+  if (e.target.classList.contains("item1")) {
+    historyView.style.display = "block";
+    historyView.textContent = "There's no history yet";
+    memoryView.style.display = "none";
+    console.log("karim");
+  }
+
+  if (e.target.classList.contains("item2")) {
+    memoryView.style.display = "block";
+    memoryView.textContent = "reeeeeeeeeeeeeedi";
+    historyView.style.display = "none";
   }
 });
